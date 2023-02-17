23679598036ad23642c6fdf0b6166e33c710f030
Tanix <91118560+Tanix98@users.noreply.github.com>
2023-02-17 23:01:10 +0100

    create header, dropdown menu and banner

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const emailBtn = document.querySelector(".email-btn");
+const openBtn = document.querySelector("#open-btn");
+const closeBtn = document.querySelector("#close-btn");
+const dropdownMenu = document.querySelector(".socials-mobile");
+const dropdownItems = document.getElementsByClassName("dropdown");
+
+/**
+ * Copies the email address to the users clipboard when button is clicked
+ */
+function emailCopy() {
+    navigator.clipboard.writeText("oysteinrostvik@protonmail.com");
+    alert("Email copied!");
+}
+emailBtn.addEventListener("click", emailCopy);
+
+/**
+ * Every time the page gets clicked, checks the clicked elements id and either opens or closes the dropdown menu
+ * Does nothing if the clicked element has no id or wrong id
+ */
+document.addEventListener("click", function (e) {
+  if (e.target.getAttribute('id') == "open-btn") {
+    for (let i = 0; i < dropdownItems.length; i++) {
+        dropdownItems[i].style.display = "block";
+    }
+    openBtn.style.display = "none";
+    dropdownMenu.style.backgroundColor = "#855BB8";
+  }
+  if (e.target.getAttribute('id') == "close-btn") {
+    for (let i = 0; i < dropdownItems.length; i++) {
+        dropdownItems[i].style.display = "none";
+    }
+    openBtn.style.display = "block";
+    dropdownMenu.style.backgroundColor = "";
+  }
+});
