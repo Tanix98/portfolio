5a64d3b5f4446037fe83f195bfa300fcc2968f88
Tanix <91118560+Tanix98@users.noreply.github.com>
2023-06-03 20:11:24 +0200

    improve accessibility with prefers-reduced-motion, clean up css code, add new projects to projects showcase, use mobile-first approach for media queries

---
+++ src/js/buttons.js
@@ -0,0 +1,82 @@
+//  Holidaze buttons
+const holidazeDemoBtn = document.querySelector('#holidaze-demo-btn');
+holidazeDemoBtn.addEventListener('click', () => {
+    window.open('https://holidaze-98.netlify.app/?page=1', '_blank');
+});
+holidazeDemoBtn.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        window.open('https://holidaze-98.netlify.app/?page=1', '_blank');
+    }
+});
+const holidazeRepoBtn = document.querySelector('#holidaze-repo-btn');
+holidazeRepoBtn.addEventListener('click', () => {
+    window.open('https://github.com/Tanix98/project-exam-2', '_blank');
+});
+holidazeRepoBtn.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        window.open('https://github.com/Tanix98/project-exam-2', '_blank');
+    }
+});
+
+// Scandinavian Auction House buttons
+const sahDemoBtn = document.querySelector('#sah-demo-btn');
+sahDemoBtn.addEventListener('click', () => {
+    window.open('https://scandinavian-auction-house.netlify.app', '_blank');
+});
+sahDemoBtn.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        window.open('https://scandinavian-auction-house.netlify.app', '_blank');
+    }
+});
+const sahRepoBtn = document.querySelector('#sah-repo-btn');
+sahRepoBtn.addEventListener('click', () => {
+    window.open('https://github.com/Tanix98/semester-project-2', '_blank');
+});
+sahRepoBtn.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        window.open('https://github.com/Tanix98/semester-project-2', '_blank');
+    }
+});
+
+// Tabernia buttons
+const taberniaDemoBtn = document.querySelector('#tabernia-demo-btn');
+taberniaDemoBtn.addEventListener('click', () => {
+    window.open('https://tabernia.netlify.app', '_blank');
+});
+taberniaDemoBtn.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        window.open('https://tabernia.netlify.app', '_blank');
+    }
+});
+const taberniaRepoBtn = document.querySelector('#tabernia-repo-btn');
+taberniaRepoBtn.addEventListener('click', () => {
+    window.open('https://github.com/Tanix98/front-end-frameworks-ca', '_blank');
+});
+taberniaRepoBtn.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        window.open(
+            'https://github.com/Tanix98/front-end-frameworks-ca',
+            '_blank'
+        );
+    }
+});
+
+// Snake Game buttons
+const snakeDemoBtn = document.querySelector('#snake-demo-btn');
+snakeDemoBtn.addEventListener('click', () => {
+    window.open('https://tanix98.github.io/snake-game/', '_blank');
+});
+snakeDemoBtn.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        window.open('https://tanix98.github.io/snake-game/', '_blank');
+    }
+});
+const snakeRepoBtn = document.querySelector('#snake-repo-btn');
+snakeRepoBtn.addEventListener('click', () => {
+    window.open('https://github.com/Tanix98/snake-game', '_blank');
+});
+snakeRepoBtn.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        window.open('https://github.com/Tanix98/snake-game', '_blank');
+    }
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,21 @@
-const emailBtn = document.getElementsByClassName("email-btn");
-const emailCopyTxt = document.querySelector("#email-copy-txt");
+const emailBtn = document.getElementsByClassName('email-btn');
+const emailCopyTxt = document.querySelector('#email-copy-txt');
 
 /**
  * Copies the email address to the users clipboard when button is clicked
  * Also displays a message to notify the user
  */
 function emailCopy() {
-    navigator.clipboard.writeText("oysteinrostvik@protonmail.com");
-    emailCopyTxt.classList.toggle("hidden");
-    emailCopyTxt.style.display = "flex";
+    navigator.clipboard.writeText('oysteinrostvik@protonmail.com');
+    emailCopyTxt.classList.toggle('hidden');
+    emailCopyTxt.style.display = 'flex';
     setTimeout(() => {
-      emailCopyTxt.classList.toggle("hidden");
-    }, "1300")
-    emailCopyTxt.classList.add("mystyle");
+        emailCopyTxt.classList.toggle('hidden');
+    }, '1300');
+    emailCopyTxt.classList.add('mystyle');
 }
 for (i = 0; i < emailBtn.length; i++) {
-  emailBtn[i].addEventListener("click", emailCopy);
+    emailBtn[i].addEventListener('click', emailCopy);
 }
 
 // Create divider over every post after the first one
@@ -25,30 +25,13 @@ for (let i = 1; i < projects.length; i++) {
 }*/
 
 // Reverse order of every other project
-const projects = document.getElementsByClassName("project");
+const projects = document.getElementsByClassName('project');
 for (let i = 1; i < projects.length; i += 2) {
-  projects[i].classList.add("reverse");
+    projects[i].classList.add('reverse');
 }
-
-const snakeDemoBtn = document.querySelector("#snake-demo-btn");
-snakeDemoBtn.addEventListener("click", () => {
-  window.open("https://tanix98.github.io/snake-game/", "_blank");
-});
-snakeDemoBtn.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
-    window.open("https://tanix98.github.io/snake-game/", "_blank");
-  }
-});
-const snakeRepoBtn = document.querySelector("#snake-repo-btn");
-snakeRepoBtn.addEventListener("click", () => {
-  window.open("https://github.com/Tanix98/snake-game", "_blank");
-});
-snakeRepoBtn.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
-    window.open("https://github.com/Tanix98/snake-game", "_blank");
-  }
-});
 
 // Keep copyright year up to date
 const currentDate = new Date();
-document.querySelector("#copyright-text").innerHTML = `© Øystein Røstvik, ${currentDate.getFullYear()}`;
+document.querySelector(
+    '#copyright-text'
+).innerHTML = `© Øystein Røstvik, ${currentDate.getFullYear()}`;
