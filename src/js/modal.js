3ef9c48908734555d77cb186629bc10cf0422f82
Tanix <91118560+Tanix98@users.noreply.github.com>
2023-06-06 19:53:18 +0200

    text live version

--- src/js/modal.js
+++ src/js/modal.js
@@ -38,14 +38,17 @@ function isBefore() {
 }
 const yearString = JSON.stringify(currentDateFull.getFullYear());
 const currentYear = Number(yearString.substring(2, 4));
+console.log('is this running?');
 if (isBefore()) {
     const myAge = currentYear + 1;
     document.querySelector(
         '#first-paragraph'
     ).innerHTML = `Hello, I’m a ${myAge} year old front-end dev from Tromsø 👋`;
+    console.log('hey');
 } else {
     const myAge = currentYear + 2;
     document.querySelector(
         '#first-paragraph'
     ).innerHTML = `Hello, I’m a ${myAge} year old front-end dev from Tromsø 👋`;
+    console.log('hello');
 }
