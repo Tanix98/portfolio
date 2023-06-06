61f02dc978284c4538c4689bf5c880d0bd1ce39d
Tanix <91118560+Tanix98@users.noreply.github.com>
2023-06-06 20:47:26 +0200

    text live version

--- src/js/modal.js
+++ src/js/modal.js
@@ -27,28 +27,31 @@ function closeModal() {
 }
 
 // Keep age up to date, either adds one or two to the current year and sets the modal's first paragraph text
-const currentDateFull = new Date();
-const currentMonth = currentDateFull.getMonth() + 1;
-const currentDay = currentDateFull.getDate();
-const dayMonth = `${currentMonth}.${currentDay}`;
-const dayMonthNumber = Number(dayMonth);
-const birthDate = Number(9.25);
-function isBefore() {
-    return dayMonthNumber < birthDate;
+function setModalFirstParagraph() {
+    const currentDateFull = new Date();
+    const currentMonth = currentDateFull.getMonth() + 1;
+    const currentDay = currentDateFull.getDate();
+    const dayMonth = `${currentMonth}.${currentDay}`;
+    const dayMonthNumber = Number(dayMonth);
+    const birthDate = Number(9.25);
+    function isBefore() {
+        return dayMonthNumber < birthDate;
+    }
+    const yearString = JSON.stringify(currentDateFull.getFullYear());
+    const currentYear = Number(yearString.substring(2, 4));
+    console.log('is this running?');
+    if (isBefore()) {
+        const myAge = currentYear + 1;
+        document.querySelector(
+            '#first-paragraph'
+        ).innerHTML = `Hello, I’m a ${myAge} year old front-end dev from Tromsø 👋`;
+        console.log('hey');
+    } else {
+        const myAge = currentYear + 2;
+        document.querySelector(
+            '#first-paragraph'
+        ).innerHTML = `Hello, I’m a ${myAge} year old front-end dev from Tromsø 👋`;
+        console.log('hello');
+    }
 }
-const yearString = JSON.stringify(currentDateFull.getFullYear());
-const currentYear = Number(yearString.substring(2, 4));
-console.log('is this running?');
-if (isBefore()) {
-    const myAge = currentYear + 1;
-    document.querySelector(
-        '#first-paragraph'
-    ).innerHTML = `Hello, I’m a ${myAge} year old front-end dev from Tromsø 👋`;
-    console.log('hey');
-} else {
-    const myAge = currentYear + 2;
-    document.querySelector(
-        '#first-paragraph'
-    ).innerHTML = `Hello, I’m a ${myAge} year old front-end dev from Tromsø 👋`;
-    console.log('hello');
-}
+setModalFirstParagraph();
